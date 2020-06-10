3beeaa36138a0eb08342828ed1169b406e943710
edithturn <edithpuclla20@gmail.com>
2020-06-10 13:28:10 -0700

    :snake: add question

--- 03_single_row_keyboard.c
+++ 03_single_row_keyboard.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /*
 **There is a special keyboard with all keys in a single row.
-**Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. The time taken to move your finger from index i to index j is |i - j|.
+**Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0. 
+**To type a character, you have to move your finger to the index of the desired character. The time taken to move your finger from index i to index j is |i - j|.
 **You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
 **Example 1:
 **Input: keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
@@ -12,6 +14,7 @@
 **Input: keyboard = "pqrstuvwxyzabcdefghijklmno", word = "leetcode"
 **Output: 73
 */
+
 int return_index(char *keyboard, char c){
 	int i;
 	i = 0;
