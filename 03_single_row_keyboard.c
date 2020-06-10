ddd1dd5cdc22442df4011185ce3c79e57ce98124
edithturn <edithpuclla20@gmail.com>
2020-06-10 13:22:43 -0700

    :snake: formating

--- 03_single_row_keyboard.c
+++ 03_single_row_keyboard.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int return_index(char *keyboard, char c){
-int i;
-i = 0;
-while(keyboard[i])
-{
-	if (keyboard[i] == c)
-		return i;
-	i++;
-}
-return 0;
+	int i;
+	i = 0;
+	while(keyboard[i])
+	{
+		if (keyboard[i] == c)
+			return i;
+		i++;
+	}
+	return 0;
 }
 int calculateTime(char *keyboard, char *word){
 	int index;
@@ -32,14 +32,14 @@ int calculateTime(char *keyboard, char *word){
 }
 
 int main(){
-int time;
-char *word;
-char *keyboard;
+	int time;
+	char *word;
+	char *keyboard;
 
-keyboard = "pqrstuvwxyzabcdefghijklmno";
-word = "leetcode";
-time = 0;
-time = calculateTime(keyboard, word);
-printf("%d\n", time);
-return (0);
+	keyboard = "pqrstuvwxyzabcdefghijklmno";
+	word = "leetcode";
+	time = 0;
+	time = calculateTime(keyboard, word);
+	printf("%d\n", time);
+	return (0);
 }
