637eef172371f0ed659f7c125f2b6c10c5bd0458
edithturn <edithpuclla20@gmail.com>
2020-11-17 13:03:04 -0800

    longest common prefix

---
+++ basic_top_interview_question/python/11_longest_common_prefix.py
@@ -0,0 +1,20 @@
+# Longest Common Prefix
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+def longestCommonPrefix(strs):
+	if len(strs) == 0:
+		return ""
+	prefix = ''
+	strs = sorted(strs)
+	#print(strs)
+	for  i in strs[0]:
+		if strs[-1].startswith(prefix+i):
+			prefix += i
+	return prefix
+
+
+# Test
+strs = ["flower","flow","flight"]
+print(longestCommonPrefix(strs))
