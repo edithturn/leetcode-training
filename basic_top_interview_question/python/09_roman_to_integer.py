6519c1d09ee04f7a6d0dbb151c5a87740a20bcca
edithturn <edithpuclla20@gmail.com>
2020-11-16 21:30:55 -0800

    fixing roman to integer

--- basic_top_interview_question/python/09_roman_to_integer.py
+++ basic_top_interview_question/python/09_roman_to_integer.py
@@ -6,11 +6,11 @@ def convert_integer(roman : str):
 	value = { "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "M" : 1000}
 	while i  < len(roman):
 		if i + 1 < len(roman) and value[roman[i]] < value[roman[i + 1]] :
-			total = total + (value[roman[i + 1]]+ value[roman[i]] )
+			total = total + (value[roman[i + 1]] - value[roman[i]] )
 			i = i + 2
 		else:
 			total = total + value[roman[i]]
 			i = i + 1
 	return total
 
-print(convert_integer("XXXII"))
+print(convert_integer("XXXVI"))
