11cd0df35921ff65b88e39ceb73f79fc6b54905c
Edi <edithpuclla20@gmail.com>
2021-02-23 22:43:25 -0500

    replace

---
+++ basic_top_interview_question/python/21_element_replace.py
@@ -0,0 +1,29 @@
+# Element Replace
+# Write a method element_replace that takes in a list and a dictionary. The method should return a new list where elements of the original list are replaced with their corresponding values in the dictionary.
+
+# list1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+# dict1 = {"Serena Williams": "tennis", "LeBron James": "basketball"}
+
+# element_replace(list1, dict1) # => ["basketball", "Lionel Messi", "tennis"]
+
+# list2 = ["dog", "cat", "mouse"]
+# dict2 = {"dog": "bark", "cat": "meow", "duck": "quack"}
+
+# element_replace(list2, dict2) # => ["bark", "meow", "mouse"]
+
+
+def element_replace(_list, _dic):
+    for key, item in enumerate(_list):
+        if item in _dic:
+            _list[key] = _dic[item]
+    return _list
+
+list1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+dict1 = {"Serena Williams": "tennis", "LeBron James": "basketball"}
+
+
+list2 = ["dog", "cat", "mouse"]
+dict2 = {"dog": "bark", "cat": "meow", "duck": "quack"}
+
+print(element_replace(list1, dict1))
+print(element_replace(list2, dict2))
