890e92adda3bbf1f842fafbc6932a543741e52ce
Edi <edithpuclla20@gmail.com>
2021-03-11 22:23:30 -0500

    count emails

--- basic_top_interview_question/python/exercises_lists/exercise02.py
+++ basic_top_interview_question/python/exercises_lists/exercise02.py
@@ -1,22 +1,20 @@
-# Exercise 2: Write a program that categorizes each mail message by which day of
-# the week the commit was done. To do this look for lines that start with “From”,
-# then look for the third word and keep a running count of each of the days of the
-# week. At the end of the program print out the contents of your dictionary (order
-# does not matter).
-# Sample Line:
-# From stephen.marquard@uct.ac.za Sat Jan
-# 5 09:14:16 2008
-# Sample Execution:
-# python dow.py
-# Enter a file name: mbox-short.txt
-# {'Fri': 20, 'Thu': 6, 'Sat': 1}
+# Exercise 2: Write a program to read through a mail log, build a histogram using
+# a dictionary to count how many messages have come from each email address, and
+# print the dictionary.
+# Enter file name: mbox-short.txt
+# {'gopal.ramasammycook@gmail.com': 1, 'louis@media.berkeley.edu': 3,
+# 'cwen@iupui.edu': 5, 'antranig@caret.cam.ac.uk': 1,
+# 'rjlowe@iupui.edu': 2, 'gsilver@umich.edu': 3,
+# 'david.horwitz@uct.ac.za': 4, 'wagnermr@iupui.edu': 1,
+# 'zqian@umich.edu': 4, 'stephen.marquard@uct.ac.za': 2,
+# 'ray@media.berkeley.edu': 1}
 
 filename = input("Enter a file name: ")
 fhand = open(filename, 'r')
+email_addresses = {}
 
-days = {}
 for line in fhand:
     if line.startswith("From "):
-        day = line.split()[2]
-        days[day] = days.get(day, 0) + 1
-print(days)
+        email = line.split()[1]
+        email_addresses[email] = email_addresses.get(email, 0) + 1
+print(email_addresses)
---
+++ basic_top_interview_question/python/exercises_lists/exercise03.py
@@ -0,0 +1,10 @@
+# Exercise 4: Add code to the above program to figure out who has the most
+# messages in the file.
+# After all the data has been read and the dictionary has been created, look through
+# the dictionary using a maximum loop (see Section [maximumloop]) to find who
+# has the most messages and print how many messages the person has.
+# Enter a file name: mbox-short.txt
+# cwen@iupui.edu 5
+# Enter a file name: mbox.txt
+# zqian@umich.edu 195
+
