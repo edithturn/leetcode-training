92199365e0176432c98457be38fb504e7705e5d9
Edith <edithpuclla20@gmail.com>
2021-05-04 08:55:23 -0500

    valid palindrome

---
+++ basic_top_interview_question/python/30_valid_palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrome(s):
+    alphanumeric = ""
+    reversed_alphanumeric = ""
+    for character in s:
+        if character.isalnum():
+            alphanumeric += character.lower()
+    for c in alphanumeric[::-1]:
+        reversed_alphanumeric += c
+    return  reversed_alphanumeric == alphanumeric
+
+string =  "A man, a plan, a canal: Panama"
+print(isPalindrome(string))
