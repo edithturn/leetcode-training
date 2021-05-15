c2b2a979b1124ed85e57d1152546bc73d7c41dd2
Edith <edithpuclla20@gmail.com>
2021-05-14 20:18:39 -0500

    linked list cycle

--- basic_top_interview_question/python/44_linked_list_cycle.py
+++ basic_top_interview_question/python/44_linked_list_cycle.py
@@ -7,6 +7,24 @@ class LinkedList:
     """
     two-pointer technique.
     """
+    def get_node(self, head, pos):
+        if pos != -1:
+            p = 0
+            ptr = head
+            while p < pos:
+                ptr = ptr.next
+                p += 1
+            return ptr
+    def hasCycle(self, head):
+        llist = set()
+        current = self.head
+        while current:
+            if current in llist:
+                return True
+            llist.add(current)
+            current = current.next
+        return False
+
     def print_list(self):
         _list = self.head
         while _list is not None:
@@ -17,16 +35,18 @@ class LinkedList:
 my_list = LinkedList()
 
 # Creating 03 nodes
-node1 = ListNode(3)
+node1 = ListNode(1)
 node2 = ListNode(2)
 node3 = ListNode(0)
-node4 = ListNode(4)
+node4 = ListNode(-4)
 
 
 my_list.head = node1
 node1.next = node2
 node2.next = node3 
 node3.next = node4
-#node4.next = node2
+pos = 1
+node4.next = my_list.get_node(my_list.head, pos)
 
-my_list.print_list()
+#my_list.print_list()
+print(my_list.hasCycle(my_list.head))
