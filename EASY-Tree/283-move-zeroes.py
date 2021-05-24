cedbd3eb05ec41e3dc6f975ab26bb6995f711a74
Edith <edithpuclla20@gmail.com>
2021-05-23 20:48:00 -0500

    move zeroes

---
+++ EASY-Tree/283-move-zeroes.py
@@ -0,0 +1,45 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Note that you must do this in-place without making a copy of the array.
+
+# Example 1:
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [0]
+
+def moveZeroes(nums):
+    """
+    First Approach Brute Force: For iterations:
+    - To know the amount of zeros that we have
+    - second: to add into a list al the non zeros number
+    - four: Add into the list the amoung of zeros that we already know
+    - five: change the values of the originas list with our final list
+    ||======= Big O ======= ||
+    * Time complexity : O(n) 
+    * Space complexity: O(n)
+    """
+    count = 0
+    llist = []
+    for n in nums:
+        if n == 0:
+            count += 1
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            llist.append(nums[i])
+    
+    while count > 0:
+        llist.append(0)
+        count -= 1
+    
+    for i in range(len(nums)): 
+        nums[i] = llist[i]
+    
+    return nums
+    
+
+nums1 = [0,1,0,3,12]
+nums2 = [0]
+print(moveZeroes(nums1))
+print(moveZeroes(nums2))
