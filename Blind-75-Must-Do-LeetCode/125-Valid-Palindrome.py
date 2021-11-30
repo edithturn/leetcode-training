824e7a382300dff82b9a0872ac8b226deb2ec456
edithturn <edypuclla@gmail.com>
2021-11-29 22:33:49 -0500

    Valid Palindrome

---
+++ Blind-75-Must-Do-LeetCode/125-Valid-Palindrome.py
@@ -0,0 +1,24 @@
+    def isPalindrome(self, s: str) -> bool:
+        """
+        Two pointers technique: Left and Rigth pointers
+        Big (O)
+        Time: O(n)
+        Space: O(1)
+        """
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            if  not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left  += 1
+                right -= 1
+     
+        return True
+
+        
